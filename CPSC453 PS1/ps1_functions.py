edec372c95c99fc2c50612ae690dd5add52ba523
idjoannchen <joannamcat@gmail.com>
2020-10-06 23:58:42 -0400

    add pset 1

--- CPSC453 PS1/ps1_functions.py
+++ CPSC453 PS1/ps1_functions.py
@@ -11,6 +11,8 @@
 # import required libraries
 import numpy as np
 import codecs, json
+import scipy
+from scipy import linalg
 
 ##############################
 # Predefined functions
@@ -54,8 +56,13 @@ def compute_distances(X):
 
     Outputs:
         D       a numpy array of size n x n containing the euclidean distances between points
-
     '''
+    n = len(X)
+    D = np.zeros([n, n])
+
+    for i in range(n):
+        for j in range(n):
+            D[i, j] = np.sqrt(sum((X[i] - X[j])**2)) # Euclidean distance
 
     # return distance matrix
     return D
@@ -76,10 +83,39 @@ def compute_affinity_matrix(D, kernel_type, sigma=None, k=None):
     Outputs:
         W       a numpy array of size n x n that is the affinity matrix
 
+    Examples:
+        W = compute_affinity_matrix(D, "adaptive", None, 5)
+        W = compute_affinity_matrix(D, "gaussian", 3, None)
     '''
+    if kernel_type=='gaussian':
+      if sigma <= 0:
+        raise ValueError('sigma must be a positive number')
+      else:
+        W = np.exp(-D*D/(sigma**2))
+             
+    if kernel_type=='adaptive':
+        if k < 0 or type(k) != int:
+          raise ValueError('k must be a positive integer')
+        else:
+          W = np.zeros(shape = np.shape(D))
+          for i in range(len(D)):
+            sigma_x_i = np.sort(D[i])[k] # we use k instead of k-1 because self doesn't count as self's neighbor
+            
+            for j in range(len(D)):
+              # acquire distance from x_i to x_j, k nearest neighbor for the ith row and jth row
+              dist = D[i, j]
+              
+              sigma_x_j = np.sort(D[j])[k]
+
+              # compute adaptive knn kernel based on the formula
+              first_exp_term = np.exp((-1 * (dist**2)) / (sigma_x_i ** 2))
+              second_exp_term = np.exp((-1 * (dist**2)) / (sigma_x_j ** 2))
+              W[i, j] = 0.5 * (first_exp_term + second_exp_term)
 
     # return the affinity matrix
     return W
+
+
 
 
 def diff_map_info(W):
@@ -90,30 +126,45 @@ def diff_map_info(W):
         W           a numpy array of size n x n containing the affinities between points
 
     Outputs:
-
         diff_vec    a numpy array of size n x n-1 containing the n-1 nontrivial eigenvectors of Markov matrix as columns
         diff_eig    a numpy array of size n-1 containing the n-1 nontrivial eigenvalues of Markov matrix
-
         We assume the convention that the coordinates in the diffusion vectors are in descending order
         according to eigenvalues.
     '''
 
-    # return the info for diffusion maps
-    return diff_vec, diff_eig
+    # get symmetric matrix M_s
+    D = np.diag(W.sum(axis=1)) # degree matrix of W = sum of W-th rows
+
+    semi_D = scipy.linalg.fractional_matrix_power(D, -1/2) # power of (-1/2)
+    M_s = semi_D @ W @ semi_D
+    eig_val, eig_vector = np.linalg.eigh(M_s) # eigenpairs of M_s
+
+    eig_vector_normalized = semi_D.dot(eig_vector) / np.linalg.norm(semi_D.dot(eig_vector), axis = 0) # normalzied eigenvector
+
+    # discard trivial eigenvalue and eigenvector
+    nontrivial_eig_val = eig_val[:-1]
+    nontrivial_eig_vector = eig_vector_normalized[:,:-1] # discard last column
+
+    # ascending order
+    diff_eig = nontrivial_eig_val[::-1]
+    diff_vec = np.flip(nontrivial_eig_vector, axis = 1)
+
+    #return the info for diffusion maps
+    return diff_vec, diff_eig 
 
 
 def get_diff_map(diff_vec, diff_eig, t):
     '''
     Construct a diffusion map at t from eigenvalues and eigenvectors of Markov matrix
-
     Inputs:
         diff_vec    a numpy array of size n x n-1 containing the n-1 nontrivial eigenvectors of Markov matrix as columns
         diff_eig    a numpy array of size n-1 containing the n-1 nontrivial eigenvalues of Markov matrix
         t           diffusion time parameter t
-
     Outputs:
         diff_map    a numpy array of size n x n-1, the diffusion map defined for t
     '''
 
-    return diff_map
+    diff_eig_power_t = diff_eig**t # t(step-size)-th power eigenvalues
+    diff_map = diff_eig_power_t * diff_vec
 
+    return diff_map
